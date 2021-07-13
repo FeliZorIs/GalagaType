9cc5f916a87becb830639e9876fbe2496bfc26e9
Oris Feliz <felizoris@gmail.com>
2021-07-13 15:11:29 -0400

    shooting works and bullet differentiates between enemies, might mess things up later so quick commit

--- Assets/Entities/Bullets/Bullet_Behaviour.cs
+++ Assets/Entities/Bullets/Bullet_Behaviour.cs
@@ -6,21 +6,27 @@ public class Bullet_Behaviour : MonoBehaviour
 {
     public GameObject bulletImpact;
     public float speed;
-    Vector2 direction;
+
+    Rigidbody2D rb2d;
+    Vector2 dir;
 
     Vector2 screenBounds;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.up * speed;
+        dir = rb2d.velocity;
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-        direction = Vector2.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+
 
         if (this.transform.position.y >= screenBounds.y)
         {
@@ -34,6 +40,12 @@ public class Bullet_Behaviour : MonoBehaviour
         {
             Instantiate(bulletImpact, this.transform.position, Quaternion.identity);
             Destroy(this.gameObject);
+
+            /*Vector2 wallNormal = collision.contacts[0].normal;
+            dir = Vector2.Reflect(rb2d.velocity, wallNormal).normalized;
+
+            rb2d.velocity = dir * speed;
+            */
         }
     }
 }
